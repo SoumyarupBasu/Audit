c18413bff4ac1faf3ddb1dd10705938869ed1eb1
Saksham Kamboj <saksham@ekvayu.com>
2025-12-26 14:39:58 +0530

    add breadcrumbs

--- src/pages/profileManagement/EditProfile.jsx
+++ src/pages/profileManagement/EditProfile.jsx
@@ -1,7 +1,16 @@
-import React from "react";
-
 function EditProfile() {
-  return <div>Edit profile page design here</div>;
+  return (
+    <div className="users-container">
+      <div className="section">
+        <div className="section-head">
+          <div className="section-title">Update profile</div>
+        </div>
+        <div className="section-body">
+          <p>Profile management content will go here...</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default EditProfile;
--- src/pages/profileManagement/Profile.jsx
+++ src/pages/profileManagement/Profile.jsx
@@ -1,7 +1,16 @@
-import React from "react";
-
 function Profile() {
-  return <div>Profile design here</div>;
+  return (
+    <div className="users-container">
+      <div className="section">
+        <div className="section-head">
+          <div className="section-title">Profile</div>
+        </div>
+        <div className="section-body">
+          <p>Profile content will go here...</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
